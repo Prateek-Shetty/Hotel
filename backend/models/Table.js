daee907c60ef781c964a7b8aaafede0454caf1af
Prateek-Shetty <prateekshetty2552@gmail.com>
2025-06-09 23:11:01 +0530

    Clear

--- backend/models/Table.js
+++
@@ -1,9 +0,0 @@
-// models/Table.js
-const mongoose = require('mongoose');
-
-const tableSchema = new mongoose.Schema({
-  number: Number,
-  capacity: Number,
-  status: { type: String, default: 'available' },
-});
-module.exports = mongoose.model('Table', tableSchema);
--- backend/routes/hotelRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getHotels } = require('../controllers/hotelController');
-
-router.get('/', getHotels);
-
-module.exports = router;
